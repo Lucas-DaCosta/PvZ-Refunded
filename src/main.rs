e1eccbde7e59e409de91edeaebde7f6b42609a86
Lucas-DaCosta <lucas.dacosta@edu.ecole-89.com>
2026-06-01 18:44:31 +0200

    move all player related functions to player.rs

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ use bevy::{
 use bevy_rapier3d::prelude::*;
 
 mod balls;
-
+mod player;
 use crate::balls::*;
+use crate::player::*;
 
 fn round_to(value: f32, decimal_places: i32) -> f32 {
     let factor: f32 = 10.0_f32.powi(decimal_places);
@@ -86,27 +87,6 @@ fn main() {
         current: 0.,
     });
     app.run();
-}
-
-#[derive(Component)]
-struct Player {
-    speed: f32,
-    creative: bool,
-    velocity: Vec3,
-    sneaking: bool,
-    audios: bool,
-}
-
-impl Default for Player {
-    fn default() -> Self {
-        Player {
-            speed: 50.,
-            creative: false,
-            velocity: Vec3::Y * 40.,
-            sneaking: false,
-            audios: true,
-        }
-    }
 }
 
 #[derive(Event, Deref)]
@@ -731,55 +711,6 @@ fn toggle_grab(
     }
 }
 
-fn player_move(
-    player: Single<(&mut Transform, &Player, &mut Velocity), With<Player>>,
-    input: Res<ButtonInput<KeyCode>>,
-    mouse_input: Res<ButtonInput<MouseButton>>,
-    time: Res<Time>,
-) {
-    let speed_multiplier = if input.pressed(KeyCode::ShiftLeft) {
-        2.
-    } else {
-        1.
-    };
-    let mut delta = Vec3::ZERO;
-    let (mut transform, player_data, mut velocity) = player.into_inner();
-    if input.pressed(KeyCode::KeyA) {
-        delta.x -= 1.;
-    }
-    if input.pressed(KeyCode::KeyD) {
-        delta.x += 1.;
-    }
-    if input.pressed(KeyCode::KeyW) {
-        delta.z += 1.;
-    }
-    if input.pressed(KeyCode::KeyS) {
-        delta.z -= 1.;
-    }
-    let forward = transform.forward().as_vec3() * delta.z;
-    let right = transform.right().as_vec3() * delta.x;
-    let mut to_move = forward + right;
-    to_move.y = 0.;
-    // fly or jump depending on player gamemode
-    if player_data.creative && input.pressed(KeyCode::Space) {
-        to_move.y += 1.;
-    }
-    if player_data.creative
-        && (input.pressed(KeyCode::ControlLeft) || mouse_input.pressed(MouseButton::Forward))
-        && !player_data.sneaking
-    {
-        to_move.y -= 1.;
-    }
-    to_move = to_move.normalize_or_zero();
-    if player_data.creative {
-        transform.translation += to_move * time.delta_secs() * player_data.speed * speed_multiplier;
-    } else {
-        let futur_move = to_move * player_data.speed * speed_multiplier;
-        velocity.linvel.x = futur_move.x;
-        velocity.linvel.z = futur_move.z;
-    }
-}
-
 fn switch_gamemode(
     player: Single<(Entity, &mut Player, &mut Velocity), With<Player>>,
     input: Res<ButtonInput<KeyCode>>,
@@ -798,55 +729,6 @@ fn switch_gamemode(
                 .entity(entity)
                 .remove::<(RigidBodyDisabled, ColliderDisabled)>();
         }
-    }
-}
-
-fn player_sneak(
-    player: Single<(&mut Player, &mut Transform, Entity), With<Player>>,
-    mut commands: Commands,
-    input: Res<ButtonInput<KeyCode>>,
-    mouse_input: Res<ButtonInput<MouseButton>>,
-) {
-    let (mut player_data, mut transform, entity) = player.into_inner();
-    if !player_data.creative
-        && (input.just_pressed(KeyCode::ControlLeft)
-            || mouse_input.just_pressed(MouseButton::Forward))
-        && !player_data.sneaking
-    {
-        player_data.speed *= 0.25;
-        player_data.sneaking = true;
-        commands
-            .entity(entity)
-            .insert(Collider::cuboid(2.5, 2.5, 2.5));
-        transform.translation.y -= 1.25;
-    } else if !player_data.creative
-        && player_data.sneaking
-        && (input.just_released(KeyCode::ControlLeft)
-            || mouse_input.just_released(MouseButton::Forward))
-    {
-        player_data.speed *= 4.;
-        player_data.sneaking = false;
-        commands
-            .entity(entity)
-            .insert(Collider::cuboid(2.5, 5., 2.5));
-        transform.translation.y = 0.;
-    }
-}
-
-fn player_jump(
-    player: Single<(&mut Player, &mut Velocity), With<Player>>,
-    mut commands: Commands,
-    input: Res<ButtonInput<KeyCode>>,
-    sounds: Res<SoundEffects>,
-) {
-    let (player_data, mut velocity) = player.into_inner();
-    if !player_data.creative && input.pressed(KeyCode::Space) && velocity.linvel.y == 0. {
-        velocity.linvel.y = player_data.velocity.y;
-        commands.spawn((
-            InGameSfx,
-            AudioPlayer::new(sounds.jump.clone()),
-            PlaybackSettings::DESPAWN.with_volume(Volume::Linear(0.1)),
-        ));
     }
 }
 
---
+++ src/player.rs
@@ -0,0 +1,124 @@
+use bevy::{audio::Volume, prelude::*};
+
+use bevy_rapier3d::prelude::*;
+
+use crate::{InGameSfx, SoundEffects};
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+    pub creative: bool,
+    pub velocity: Vec3,
+    pub sneaking: bool,
+    pub audios: bool,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Player {
+            speed: 50.,
+            creative: false,
+            velocity: Vec3::Y * 40.,
+            sneaking: false,
+            audios: true,
+        }
+    }
+}
+
+pub fn player_move(
+    player: Single<(&mut Transform, &Player, &mut Velocity), With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+    time: Res<Time>,
+) {
+    let speed_multiplier = if input.pressed(KeyCode::ShiftLeft) {
+        2.
+    } else {
+        1.
+    };
+    let mut delta = Vec3::ZERO;
+    let (mut transform, player_data, mut velocity) = player.into_inner();
+    if input.pressed(KeyCode::KeyA) {
+        delta.x -= 1.;
+    }
+    if input.pressed(KeyCode::KeyD) {
+        delta.x += 1.;
+    }
+    if input.pressed(KeyCode::KeyW) {
+        delta.z += 1.;
+    }
+    if input.pressed(KeyCode::KeyS) {
+        delta.z -= 1.;
+    }
+    let forward = transform.forward().as_vec3() * delta.z;
+    let right = transform.right().as_vec3() * delta.x;
+    let mut to_move = forward + right;
+    to_move.y = 0.;
+    // fly or jump depending on player gamemode
+    if player_data.creative && input.pressed(KeyCode::Space) {
+        to_move.y += 1.;
+    }
+    if player_data.creative
+        && (input.pressed(KeyCode::ControlLeft) || mouse_input.pressed(MouseButton::Forward))
+        && !player_data.sneaking
+    {
+        to_move.y -= 1.;
+    }
+    to_move = to_move.normalize_or_zero();
+    if player_data.creative {
+        transform.translation += to_move * time.delta_secs() * player_data.speed * speed_multiplier;
+    } else {
+        let futur_move = to_move * player_data.speed * speed_multiplier;
+        velocity.linvel.x = futur_move.x;
+        velocity.linvel.z = futur_move.z;
+    }
+}
+
+pub fn player_sneak(
+    player: Single<(&mut Player, &mut Transform, Entity), With<Player>>,
+    mut commands: Commands,
+    input: Res<ButtonInput<KeyCode>>,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+) {
+    let (mut player_data, mut transform, entity) = player.into_inner();
+    if !player_data.creative
+        && (input.just_pressed(KeyCode::ControlLeft)
+            || mouse_input.just_pressed(MouseButton::Forward))
+        && !player_data.sneaking
+    {
+        player_data.speed *= 0.25;
+        player_data.sneaking = true;
+        commands
+            .entity(entity)
+            .insert(Collider::cuboid(2.5, 2.5, 2.5));
+        transform.translation.y -= 1.25;
+    } else if !player_data.creative
+        && player_data.sneaking
+        && (input.just_released(KeyCode::ControlLeft)
+            || mouse_input.just_released(MouseButton::Forward))
+    {
+        player_data.speed *= 4.;
+        player_data.sneaking = false;
+        commands
+            .entity(entity)
+            .insert(Collider::cuboid(2.5, 5., 2.5));
+        transform.translation.y = 0.;
+    }
+}
+
+pub fn player_jump(
+    player: Single<(&mut Player, &mut Velocity), With<Player>>,
+    mut commands: Commands,
+    input: Res<ButtonInput<KeyCode>>,
+    sounds: Res<SoundEffects>,
+) {
+    let (player_data, mut velocity) = player.into_inner();
+    if !player_data.creative && input.pressed(KeyCode::Space) && velocity.linvel.y == 0. {
+        velocity.linvel.y = player_data.velocity.y;
+        commands.spawn((
+            InGameSfx,
+            AudioPlayer::new(sounds.jump.clone()),
+            PlaybackSettings::DESPAWN.with_volume(Volume::Linear(0.1)),
+        ));
+    }
+}
